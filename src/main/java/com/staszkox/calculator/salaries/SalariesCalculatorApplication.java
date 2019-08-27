c02c94b06c12fdffb72843f20b20b79c9b81c9bb
stacherzakp <pawel.stacherzak@gmail.com>
2019-08-27 18:38:21 +0200

    Project skeleton

---
+++ src/main/java/com/staszkox/calculator/salaries/SalariesCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.staszkox.calculator.salaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalariesCalculatorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SalariesCalculatorApplication.class, args);
+    }
+}
