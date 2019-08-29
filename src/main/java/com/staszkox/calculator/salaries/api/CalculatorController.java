121ccf6bf754e90f0b28553a9502c6ffd76706d5
stacherzakp <pawel.stacherzak@gmail.com>
2019-08-29 21:08:02 +0200

    Basic user interface

--- src/main/java/com/staszkox/calculator/salaries/api/CalculatorController.java
+++ src/main/java/com/staszkox/calculator/salaries/api/CalculatorController.java
@@ -5,10 +5,7 @@ import com.staszkox.calculator.salaries.api.model.NetCalculationRequest;
 import com.staszkox.calculator.salaries.api.model.NetCalculationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calculator")
@@ -21,12 +18,14 @@ public class CalculatorController {
         this.serviceAdapter = serviceAdapter;
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/net")
     public ResponseEntity<NetCalculationResponse> netSalary(NetCalculationRequest request) {
         NetCalculationResponse netSalary = serviceAdapter.calculateNetSalary(request);
         return ResponseEntity.ok(netSalary);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/config")
     public ResponseEntity<ConfigurationResponse> configuration() {
         ConfigurationResponse configuration = serviceAdapter.getCalculatorConfiguration();
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,31 @@
+var app = angular.module('calculator', []);
+
+app.controller('ctrl', function ($scope, $http) {
+
+    $scope.isConfigLoaded = false;
+
+    $http.get("calculator/config")
+        .then(function (response) {
+            $scope.systemCurrency = response.data.systemCurrency;
+            $scope.countries = response.data.countries;
+            $scope.isConfigLoaded = true;
+        }, function (response) {
+            $scope.message = "System error."
+        });
+
+    $scope.calculate = function (income, selectedCountry) {
+
+        $http.post("calculator/net", {
+            dailyIncome: income,
+            currency: selectedCountry.currency
+        }, {headers: {'Content-Type': 'application/json'}})
+            .then(function (response) {
+                $scope.calculatedSalary = response.data.salary;
+                $scope.calculatedSalaryCurrency = response.data.currency;
+                $scope.message = "Salary calculated successfully."
+            }, function (response) {
+                $scope.message = "Calculation error."
+            });
+
+    };
+});
